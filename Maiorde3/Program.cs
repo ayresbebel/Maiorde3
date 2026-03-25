0f6129d3bfdd23429cbb3cd5254634a4b7e6bbab
Isabelle Nascimento <ayres09bebel@gmail.com>
2026-03-25 20:04:16 -0300

    Implementação inicial

---
+++ Maiorde3/Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<double> listNumero = new List<double>();
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write($"Digite o {i+1}° número: ");
+
+            string? num = Console.ReadLine();
+            double numeroInformado;
+
+            while (!double.TryParse(num, out numeroInformado))
+            {
+                Console.Write("Número inválido. Tente novamente: ");
+                num = Console.ReadLine();
+            }
+
+            listNumero.Add(numeroInformado);
+        }
+
+        double? maiorNumero = null;
+
+        foreach (double numero in listNumero)
+        {
+            if (maiorNumero == null || numero > maiorNumero)
+            {
+                maiorNumero = numero;
+            }
+
+        }
+
+
+        Console.WriteLine($"O maior número dos três digitados é {maiorNumero}");
+        Console.ReadLine();
+    }
+}
