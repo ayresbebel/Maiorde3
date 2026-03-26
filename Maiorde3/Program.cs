12e0ce3f04b2be35149d22494a34e1ed8b8ce518
Isabelle Nascimento <ayres09bebel@gmail.com>
2026-03-26 19:48:40 -0300

    Praticando diferentes formas de comparar os números

--- Maiorde3/Program.cs
+++ Maiorde3/Program.cs
@@ -7,7 +7,7 @@
         for (int i = 0; i < 3; i++)
         {
             Console.Write($"Digite o {i+1}° número: ");
-
+                                  
             string? num = Console.ReadLine();
             double numeroInformado;
 
@@ -20,16 +20,17 @@
             listNumero.Add(numeroInformado);
         }
 
-        double? maiorNumero = null;
+        listNumero.Reverse();
+        double? maiorNumero = listNumero.First();
 
-        foreach (double numero in listNumero)
-        {
-            if (maiorNumero == null || numero > maiorNumero)
-            {
-                maiorNumero = numero;
-            }
+        //foreach (double numero in listNumero)
+        //{
+        //    if (numero > maiorNumero)
+        //    {
+        //        maiorNumero = numero;
+        //    }
 
-        }
+        //}
 
 
         Console.WriteLine($"O maior número dos três digitados é {maiorNumero}");
